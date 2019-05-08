4cec13185dc70ae4bae6769551886047f3b1c396
Cody Garvin <cody@servalsoft.com>
2019-05-07 17:11:57 -0700

    Added landscape adaptive layouts

--- Albms/Controllers/DetailsViewController.swift
+++ Albms/Controllers/DetailsViewController.swift
@@ -12,6 +12,11 @@ class DetailsViewController: AlbmsAbstractViewController {
     
     @IBOutlet var albumImageView: UIImageView!
     @IBOutlet var viewIniTunesButton: UIButton!
+    @IBOutlet var albumNameLabel: UILabel!
+    @IBOutlet var artistLabel: UILabel!
+    @IBOutlet var genreLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var copyrightTextView: UITextView!
     
     var albumDetails: Album? {
         didSet {
@@ -24,6 +29,11 @@ class DetailsViewController: AlbmsAbstractViewController {
         
         // Set up the colors and post nib lovelies.
         configureViews()
+        
+        // Check if we loaded our data before our view was done loading
+        if albumDetails != nil {
+            loadAlbumData(albumDetails)
+        }
     }
     
     @IBAction func loadItunes() {
@@ -39,11 +49,26 @@ class DetailsViewController: AlbmsAbstractViewController {
         albumImageView.layer.cornerRadius = 10.0
         viewIniTunesButton.layer.cornerRadius = 6.0
         viewIniTunesButton.backgroundColor = UIColor.albPollution()
+        albumNameLabel.textColor = UIColor.albPeach()
+        artistLabel.textColor = UIColor.albMelon()
+        genreLabel.textColor = UIColor.albPollution()
+        dateLabel.textColor = UIColor.albPollution()
+        copyrightTextView.textColor = UIColor.albPollution()
+        copyrightTextView.backgroundColor = UIColor.clear
     }
     
     private func loadAlbumData(_ album: Album?) {
         // Could be turned into a view model
         guard let album = album, let artistThumbUrl = album.artistThumbUrl else { return }
+        
+        // Make sure we have already loaded
+        guard albumNameLabel != nil else { return }
+        
+        albumNameLabel.text = album.name
+        artistLabel.text = album.artistName
+        genreLabel.text = album.genre
+        dateLabel.text = album.releaseDate
+        copyrightTextView.text = album.copyright
         
         ALBImageCache.shared.cacheImage(urlString: artistThumbUrl, indexPath: nil) { [weak self] (cachedImage, _) in
             self?.albumImageView.image = cachedImage
